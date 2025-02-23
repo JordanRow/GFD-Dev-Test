1a2ffef8ad7f40575f2d389765d20edaf6e698a9
JordanRow <jordanrowcroft@gmail.com>
2025-02-23 23:34:04 +0000

    Added images/tailwind sizing code/slick styling

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    $(".main-promo-container").slick({
-        fade: true,
-        cssEase: "linear",
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false
-    });
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        $(".main-promo-container").slick({
+            fade: true,
+            cssEase: "linear",
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed: 3000,
+            arrows: false
+        });
+    }
 
     $(".ot-container").slick({
-        rows: 3,
-        dots: false,
+        mobileFirst:true,
+        rows: 2,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
         arrows: true,
-        appendArrows: $('.ot-arrow-container'),
-        prevArrow: $('.ot-arrow-container button.left'),
-        nextArrow: $('.ot-arrow-container button.right'),
+        appendArrows: $(".ot-arrow-container"),
+        appendDots: $(".ot-dot-container"),
+        prevArrow: $(".ot-arrow-container button.left"),
+        nextArrow: $(".ot-arrow-container button.right"),
         infinite: true,
         speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    rows: 3,
+                    infinite: true,
+                }
+            },
+        ]
     });
 });
